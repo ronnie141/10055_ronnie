095c65a0090384120fd6095759a4ba06c5bcccdc
ronnie141 <ronnie01mutembei@gmail.com>
2026-06-11 13:15:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let fName = "ronnie"
+/*let fName = "ronnie"
 let lName = "mutembei"
 console.log(fName + " " + lName)
 let marks = 400
@@ -6,6 +6,7 @@ let fTermFees = 20000
 let sTermFees = 30000
 let gpa = 7.5
 let pass = true
+let totalFees = (fTermFees + sTermFees)
 let n1 = document.getElementById("firstName")
 n1.innerHTML = fName
 let n2 = document.getElementById("lastName")
@@ -18,3 +19,35 @@ let mainfTermFees = document.getElementById("fTermFees")
 mainfTermFees.innerHTML = fTermFees
 let mainsTermFees = document.getElementById("sTermFees")
 mainsTermFees.innerHTML = sTermFees
+let TotalFees = document.getElementById("totalFees")
+TotalFees.innerHTML = totalFees
+
+*/
+
+
+let fCarDistance = 2000
+let fCarTime = 12
+let fCarSpeed = fCarDistance / fCarTime
+
+let sCarDistance = 4000
+let sCarTime = 15
+let sCarSpeed = sCarDistance / sCarTime
+
+let tCarDistance = 6000
+let tCarTime = 20
+let tCarSpeed = tCarDistance / tCarTime
+
+let mainSpeed1 = document.getElementById("speed1")
+mainSpeed1.innerHTML = Math.round(fCarSpeed)
+let mainSpeed2 = document.getElementById("speed2")
+mainSpeed2.innerHTML = Math.round(sCarSpeed)
+let mainSpeed3 = document.getElementById("speed3")
+mainSpeed3.innerHTML = tCarSpeed
+
+
+
+
+
+
+
+
