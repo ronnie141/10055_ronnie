bb8daaec0ed67aec4eccd3b572d0bf1b15bc14fe
ronnie141 <ronnie01mutembei@gmail.com>
2026-06-18 12:52:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+let popularOnNetflix = ["moneyHeist", "Narcos", "Ozark", "polygamist", "the gilr on the train"]
+console.log(popularOnNetflix[0])
+console.log(popularOnNetflix[1])
+console.log(popularOnNetflix[2])
+
+let categories = ["Action", "Drama", "Animations", "Lifestyle", "Horror", "Scifie", "thriller"]
+console.log(categories[0])
+console.log(categories[1])
+console.log(categories[2])
+console.log(categories[3])
+console.log(categories[4])
+console.log(categories[5])
+console.log(categories[6])
+
+console.log(popularOnNetflix[0] + "-" + categories[1])
+console.log(popularOnNetflix[1] + "-" + categories[1])
+console.log(popularOnNetflix[2] + "-" + categories[3])
+letfName = "John"
+letsName = "Doe"
+console.log("John" + " " + "Doe")
+
+
+let displayInterface = {
+    title: "DARK",
+    rating: 5.6,
+    seasons: 4
+}
+console.log(displayInterface.title)
+console.log(displayInterface.rating)
+console.log(displayInterface.seasons)
+
+let display = {
+    title: "moneyHeist",
+    rating: 8.5,
+    seasons: 10
+}
+console.log(display.title)
+console.log(display.rating)
+console.log(display.seasons)
+
+
+let teachingdept = [
+    { name: "ronnie", salary: 20000 },
+    { name: "fridah", salary: 15000 },
+    { name: "john", salary: 25000 }
+]
+
+console.log(teachingdept[0].name + salary)
+
+
+
+
