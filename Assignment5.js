078ce450ebcf00a7da767e9aa3a9129e5a47c52a
ronnie141 <ronnie01mutembei@gmail.com>
2026-06-22 14:00:04 +0300

    Add files via upload

---
+++ Assignment5.js
@@ -0,0 +1,22 @@
+let edible = ["bread", "crips"];
+let nonEdible = ["plastic cups"];
+let machinery = ["motorcycle engines", "electric drills", "electric saws"];
+
+console.log("Edible Goods:");
+console.log(edible);
+
+console.log("Non-Edible Goods:");
+console.log(nonEdible);
+
+console.log("Machinery:");
+console.log(machinery);
+
+document.write("<h2>Edible Goods:</h2>");
+document.write(edible + "<br></br>");
+
+document.write("<h2>Non-Edible Goods:</h2>");
+document.write(nonEdible + "<br></br>");
+
+document.write("<h2>Machinery:</h2>");
+document.write(machinery);
+
